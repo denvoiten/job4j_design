db141d267e7507e7dbb797683fcb27e9ca56a042
denvoiten <den.voiten@gmail.com>
2022-04-11 14:15:36 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -1,14 +1,24 @@
 package ru.job4j.tree;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 
 
 public class SimpleTreeTest {
+    private SimpleTree<Integer> tree;
+
+    @Before
+    public void setUp() {
+        tree = new SimpleTree<>(1);
+    }
+
     @Test
     public void when6ElFindLastThen6() {
-        Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         tree.add(1, 4);
@@ -22,7 +32,6 @@ public class SimpleTreeTest {
 
     @Test
     public void when6ElFindNotExitThenOptionEmpty() {
-        Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         assertThat(
                 tree.findBy(7).isPresent(),
@@ -32,13 +41,33 @@ public class SimpleTreeTest {
 
     @Test
     public void whenChildExistOnLeafThenNotAdd() {
-        Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
         assertFalse(tree.add(2, 6));
+    }
+
+    @Test
+    public void whenTreeIsBinary() {
+        tree.add(1, 0);
+        tree.add(1, 4);
+        tree.add(4, 3);
+        tree.add(4, 6);
+        tree.add(6, 8);
+        tree.add(6, 11);
+        assertTrue(tree.isBinary());
+    }
+
+    @Test
+    public void whenTreeIsNotBinary() {
+        tree.add(1, 0);
+        tree.add(1, 4);
+        tree.add(4, 3);
+        tree.add(4, 6);
+        tree.add(4, 8);
+        assertFalse(tree.isBinary());
     }
 
 }
