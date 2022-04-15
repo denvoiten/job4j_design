a58d573ddc4b8faf48a29d81014951d0d0a1d19b
denvoiten <den.voiten@gmail.com>
2022-04-15 12:12:10 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(multiple(10).getBytes());
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String multiple(int size) {
+        StringBuilder rsl = new StringBuilder();
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                rsl.append((row + 1) * (cell + 1)).append("\t");
+            }
+            rsl.append(System.lineSeparator());
+        }
+        return rsl.toString();
+    }
+}
+
