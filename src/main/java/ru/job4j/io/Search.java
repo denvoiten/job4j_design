d1bb5627639ffa1e53e02548dcb51364aeba0133
denvoiten <den.voiten@gmail.com>
2022-04-18 17:42:08 +0300

    5. Валидация параметров запуска. [#246865 #290532]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -19,7 +19,7 @@ public class Search {
     }
 
     public static Path validate(String[] args) throws IOException {
-        if (args.length == 0 || args.length == 1) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
         return Paths.get(args[0]);
