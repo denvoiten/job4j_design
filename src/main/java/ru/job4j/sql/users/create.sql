1dc40e7cf0e9fba1f242c063c09281d4574b2f15
denvoiten <den.voiten@gmail.com>
2022-04-28 11:25:32 +0300

    1. Схема прав пользователи и роли. [#1093 #294391]

--- src/main/java/ru/job4j/sql/users/create.sql
+++ src/main/java/ru/job4j/sql/users/create.sql
@@ -8,36 +8,16 @@ create table rules(
     name varchar(255)
 );
 
-create table role_rules(
+create table roles_rules(
     id serial primary key,
-    roles_id references roles(id),
-    rules_id references rules(id)
-);
-
-create table item(
-    id serial primary key,
-    name varchar(255),
-    users_id references users(id),
-    category_id references category(id),
-    state_id references state(id)
+    roles_id int references roles(id),
+    rules_id int references rules(id)
 );
 
 create table users(
     id serial primary key,
-    name varchar(255)
-    roles_id references roles(id)
-);
-
-create table comments(
-    id serial primary key,
     name varchar(255),
-    item_id references item(id)
-);
-
-create table attachs(
-    id serial primary key,
-    name varchar(255),
-    item_id references item(id)
+    roles_id int references roles(id)
 );
 
 create table category(
@@ -48,4 +28,24 @@ create table category(
 create table state(
     id serial primary key,
     name varchar(255)
+);
+
+create table item(
+    id serial primary key,
+    name varchar(255),
+    users_id int references users(id),
+    category_id int references category(id),
+    state_id int references state(id)
+);
+
+create table comments(
+    id serial primary key,
+    name varchar(255),
+    item_id int references item(id)
+);
+
+create table attachs(
+    id serial primary key,
+    name varchar(255),
+    item_id int references item(id)
 );
