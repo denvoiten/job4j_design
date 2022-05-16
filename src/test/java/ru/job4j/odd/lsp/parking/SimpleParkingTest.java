4fe453819e1e007abd074ddb86fef00026c672ad
denvoiten <den.voiten@gmail.com>
2022-05-16 07:47:36 +0300

    2. Парковка машин [#853 #301257]

---
+++ src/test/java/ru/job4j/odd/lsp/parking/SimpleParkingTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.odd.lsp.parking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleParkingTest {
+
+    @Test
+    public void whenCarAndPlacesAvailableThenPark() {
+        Car car = new Car();
+        Parking simpleParking = new SimpleParking(1, 0);
+        assertTrue(simpleParking.park(car));
+    }
+
+    @Test
+    public void whenTruckAndPlacesAvailableThenPark() {
+        Truck truck = new Truck(3);
+        Parking simpleParking = new SimpleParking(0, 1);
+        assertTrue(simpleParking.park(truck));
+    }
+
+    @Test
+    public void whenTruckAndPlacesNotAvailableThenNotPark() {
+        Truck truck = new Truck(2);
+        Parking simpleParking = new SimpleParking(1, 0);
+        assertFalse(simpleParking.park(truck));
+    }
+
+    @Test
+    public void whenCarAndPlacesNotAvailableThenNotPark() {
+        Car car = new Car();
+        Parking simpleParking = new SimpleParking(0, 4);
+        assertFalse(simpleParking.park(car));
+    }
+
+    @Test
+    public void whenTwoCarsOneTruckAndPlacesAvailableThenPark() {
+        Car car = new Car();
+        Car car2 = new Car();
+        Truck truck = new Truck(3);
+        Parking simpleParking = new SimpleParking(6, 0);
+        assertTrue(simpleParking.park(car));
+        assertTrue(simpleParking.park(car2));
+        assertTrue(simpleParking.park(truck));
+    }
+
+    @Test
+    public void whenTwoCarsAndPlacesNotAvailableThenNotPark() {
+        Car car = new Car();
+        Car car2 = new Car();
+        Parking simpleParking = new SimpleParking(1, 3);
+        assertTrue(simpleParking.park(car));
+        assertFalse(simpleParking.park(car2));
+    }
+}
