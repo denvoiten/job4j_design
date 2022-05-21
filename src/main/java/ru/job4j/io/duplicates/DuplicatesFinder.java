96d8c4bc670cbdb6d0229794bfb27ae991146b4a
denvoiten <den.voiten@gmail.com>
2022-05-21 21:31:11 +0300

    add log

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,20 +1,24 @@
 package ru.job4j.io.duplicates;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
 import java.util.List;
 
 public class DuplicatesFinder {
+    private static final Logger LOG = LoggerFactory.getLogger(DuplicatesFinder.class.getName());
+
     public static void main(String[] args) throws IOException {
 
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("C:\\projects"), duplicatesVisitor);
         for (List<Path> value : duplicatesVisitor.getFilePaths().values()) {
             if (value.size() > 1) {
-                value.forEach(v -> System.out
-                        .printf("Address: %s; Size: %s%n", v.toAbsolutePath(), v.toFile().length()));
+                value.forEach(v -> LOG.info(("Address: ").concat(String.valueOf(v.toAbsolutePath()))
+                        .concat(" Size: ").concat(String.valueOf(v.toFile().length()))));
             }
         }
     }
